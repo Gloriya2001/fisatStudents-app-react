75041e3b324cf29586f76e2839710323f8e36307
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-10 00:42:20 +0530

    Search done

---
+++ src/components/Search.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Search = () => {
+    return (
+        <div>
+
+            <div className="container">
+                <div className="row g-3">
+                <h2><center>Search Student</center></h2>
+                
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                      
+                        <label className="form-label">Student Name</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                
+
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                      
+                      <button className="btn btn-success">Search</button>
+
+                    </div>
+
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Search
