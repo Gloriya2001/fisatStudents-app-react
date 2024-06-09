b93c0a4c0ac279a3311a1f7ce799c1603817df7f
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-10 00:35:48 +0530

    Add done

---
+++ src/components/Add.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+
+const Add = () => {
+    return (
+        <div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <h1><center>Student Registration</center></h1>
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Id</label>
+                                <input type="text" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">First Name</label>
+                                <input type="text" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Last Name</label>
+                                <input type="text" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                                <label htmlFor="" className="form-label">College Name</label>
+                                <input type="text" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Id</label>
+                                <input type="text" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Date of Birth</label>
+                                <input type="date" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Course</label>
+                                <input type="text" className="form-control" />
+
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Mobile No.</label>
+                                <input type="text" className="form-control" />
+
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Email-Id</label>
+                                <input type="text" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Address</label>
+                                <textarea name="" id="" className="form-control"></textarea>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <button className="btn btn-success">Register</button>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    )
+}
+
+export default Add
