71bcb300af559dbb12a1bec1f965d3bd6aa55c35
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-10 00:47:04 +0530

    Delete doner

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Add from './components/Add';
 import Search from './components/Search';
+import Delete from './components/Delete';
 
 function App() {
   return (
     <div>
 
 <Add/>
- <Search/>   
+ <Search/>  
+
+<Delete/> 
     </div>
   );
 }
---
+++ src/components/Delete.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Delete = () => {
+    return (
+        <div>
+
+            <div className="container">
+                <div className="row g-3">
+                    <h2><center>Delete Student</center></h2>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+
+                        <label htmlFor="" className="form-label">Student Id</label>
+                        <input type="text" className="form-control" />
+
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <button className="btn btn-danger">Delete
+
+                        </button>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    )
+}
+
+export default Delete
